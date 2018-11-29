8b8206d8b2f0952fd10b9a1ffb7e8a6bcd94839a
simon <zerbibsimon@gmail.com>
2018-11-29 11:37:34 +0200

    rendu devoir

---
+++ my_set.h
@@ -0,0 +1,4 @@
+void get_set(int array[], int* arraySize);
+void print_set(int set[], int* arraySize);
+int checkSet(int array[], int num, int* arraySize);
+void addToSet(int array[], int num, int* arraySize);
